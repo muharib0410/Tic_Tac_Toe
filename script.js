089ec199f3394e9d08d7617b878a0415c5c7ad91
muharib0410 <abagithub0410@gmail.com>
2022-05-09 16:45:08 +0500

    commit

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 window.addEventListener('DOMContentLoaded', () => {
+     const tiles = Array.from (document.querySelectorAll('.tile'));
+     const playerDisplay= document.querySelector('.display-player');
+     const resetButton = document.querySelector('#reset');
+     const announcer = document.querySelector('.announcer');   
+
+    let board = ['', '', '', '', '', '', '', '', ''];
+    let currentPlayer = 'X';
+    let isGameActive = true;
+
+
+    const PLAYERX_WON = 'PLAYERX_WON';
+    const PLAYERO_WON = 'PLAYERO_WON';
+    const TIE = 'TIE';
+
+    const winningConditions = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ];
     
-
-
-
     resetButton.addEventListener('click', resetBoard);
 });
 
