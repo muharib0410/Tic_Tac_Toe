234883e8c4945c9accee1a55f3f7008104931337
muharib0410 <abagithub0410@gmail.com>
2022-05-09 19:35:40 +0500

    commit

--- script.js
+++ script.js
@@ -24,6 +24,56 @@ window.addEventListener('DOMContentLoaded', () => {
         [2,4,6]
     ];
 
+    function handleResulValidation() {
+        let roundWon = false;
+    for (let i = 0; i<= 7; i++) {
+        const winCondition = winningConditions[i];
+        const a = board[winCondition[0]];
+        const b = board[winCondition[1]];
+        const c = board[winCondition[2]];
+        if (a === '' || c === '') {
+            continue;
+        }
+        if (a === b && b === c) {
+            roundWon = true;
+            break;
+        }
+    }
+   
+    }
+
+    if (roundWon) {
+        announce(currentPlayer === 'X' ? PLAYERX_WON: PLAYERO_WON);
+        isGameActive = false;
+        return;
+    }
+    if (!board.includes('')) {
+        announce(TIE);
+    }
+
+
+    const announce = (type) => {
+        switch(type) {
+            case PLAYERO_WON:
+                announcer.innerHTML = 'Player <span class= "playerO>" O </span> Won';
+                break; 
+            case PLAYERX_WON:
+                announcer.innerHTML = 'Player <span class= "playerX>" X </span> Won';
+                break; 
+            case TIE: 
+                announcer.innerHTML = 'Tie';
+        }
+        announcer.classList.remove('hide');
+    }
+
+    const changePlayer = () => {
+        playerDisplay.classList.remove(`player${currentPlayer}`);
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        playerDisplay.innerText = currentPlayer;
+        playerDisplay.classList.add(`player${currentPlayer}`);
+    }
+
+
 
     const userAction =  ((tile, index) => {
         if(isValidAction(tile) && isGameActive) {
